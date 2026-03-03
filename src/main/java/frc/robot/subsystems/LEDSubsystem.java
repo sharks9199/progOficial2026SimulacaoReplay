e42da402b5e8b076683cd2005a6987c12dc57a7d
Sharks FRC #9199 <122951812+sharks9199@users.noreply.github.com>
2026-03-03 14:12:55 -0300

    Add files via upload

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,104 +1,110 @@
-
 package frc.robot.subsystems;
-
-import java.util.Optional;
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.AddressableLEDBufferView;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.LEDConstants;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
 public class LEDSubsystem extends SubsystemBase {
+
     private final AddressableLED m_led;
-    private final AddressableLEDBuffer m_buffer;
+    private final AddressableLEDBuffer ledBuffer;
 
-    private final AddressableLEDBufferView allianceView;
-    private final AddressableLEDBufferView shooterView;
-    private final AddressableLEDBufferView boostView;
+    // Suppliers mais limpos
+    private final BooleanSupplier m_readyToShootSupplier;
+    private final DoubleSupplier m_robotVelocitySupplier;
 
-    private BooleanSupplier isShooterAligned;
-    private BooleanSupplier hasTarget;
-    private DoubleSupplier currentSpeed;
+    private double m_brilho = 1.0;
 
-    public LEDSubsystem(BooleanSupplier isShooterAligned, BooleanSupplier hasTarget, DoubleSupplier currentSpeed) {
-        this.isShooterAligned = isShooterAligned;
-        this.hasTarget = hasTarget;
-        this.currentSpeed = currentSpeed;
+    private final int LED_LENGTH = 216; 
 
-        m_led = new AddressableLED(LEDConstants.kLEOPort);
-        m_buffer = new AddressableLEDBuffer(LEDConstants.kTotalLength);
-        m_led.setLength(m_buffer.getLength());
+    private final int BAR1_START = 18;
+    private final int BAR1_END = 41;
+    private final int BAR2_START = 177;
+    private final int BAR2_END = 200;
 
-        allianceView = m_buffer.createView(LEDConstants.kAllianceStart, LEDConstants.kAllianceEnd);
-        shooterView = m_buffer.createView(LEDConstants.kShooterStart, LEDConstants.kShooterEnd);
-        boostView = m_buffer.createView(LEDConstants.kBoostStart, LEDConstants.kBoostEnd);
+    private final double MAX_VELOCITY_MPS = 5.0;
 
-        m_led.setData(m_buffer);
+    public LEDSubsystem(BooleanSupplier readyToShootSupplier, DoubleSupplier robotVelocitySupplier) {
+        this.m_readyToShootSupplier = readyToShootSupplier;
+        this.m_robotVelocitySupplier = robotVelocitySupplier;
+
+        m_led = new AddressableLED(9); 
+        ledBuffer = new AddressableLEDBuffer(LED_LENGTH);
+        m_led.setLength(ledBuffer.getLength());
+
+        SmartDashboard.putNumber("LED Brilho", 1.0);
+
+        limparBuffer();
         m_led.start();
+    }
+
+    private void limparBuffer() {
+        for (var i = 0; i < ledBuffer.getLength(); i++) {
+            ledBuffer.setRGB(i, 0, 0, 0);
+        }
+    }
+
+    private void atualizarEstadoAtirador() {
+        boolean readyToShoot = m_readyToShootSupplier.getAsBoolean();
+        
+        int r = readyToShoot ? 0 : (int)(255 * m_brilho);
+        int g = readyToShoot ? (int)(255 * m_brilho) : 0;
+        int b = 0;
+
+        for (int i = 0; i < ledBuffer.getLength(); i++) {
+            if (!isInVelocityBar(i)) {
+                ledBuffer.setRGB(i, r, g, b);
+            }
+        }
+    }
+
+    private void atualizarBarrasVelocidade() {
+        double velocity = m_robotVelocitySupplier.getAsDouble();
+        double percentage = Math.max(0.0, Math.min(1.0, velocity / MAX_VELOCITY_MPS));
+
+        int r = (int)(255 * m_brilho);
+        int g = (int)(165 * m_brilho);
+        int b = 0;
+
+        // Barra 1: Lado esquerdo (Sobe do 18 ao 41) - reverse = false
+        applyBarGradient(BAR1_START, BAR1_END, percentage, r, g, b, false);
+
+        // Barra 2: Lado direito (Sobe do 200 ao 177) - reverse = true
+        applyBarGradient(BAR2_START, BAR2_END, percentage, r, g, b, true);
+    }
+
+    private void applyBarGradient(int startIdx, int endIdx, double percentage, int r, int g, int b, boolean reverse) {
+        int length = endIdx - startIdx + 1;
+        int ledsAcesos = (int) Math.round(length * percentage);
+
+        for (int i = 0; i < length; i++) {
+            int ledIndex = reverse ? (endIdx - i) : (startIdx + i);
+            
+            if (i < ledsAcesos) {
+                ledBuffer.setRGB(ledIndex, r, g, b);
+            } else {
+                ledBuffer.setRGB(ledIndex, 0, 0, 0);
+            }
+        }
+    }
+
+    private boolean isInVelocityBar(int index) {
+        return (index >= BAR1_START && index <= BAR1_END) || 
+               (index >= BAR2_START && index <= BAR2_END);
     }
 
     @Override
     public void periodic() {
-        updateAllianceColor();
-        updateShooterStatus();
-        updateSpeedBar();
+        double valorLido = SmartDashboard.getNumber("LED Brilho", 1.0);
+        m_brilho = Math.max(0.0, Math.min(1.0, valorLido));
 
-        m_led.setData(m_buffer);
-    }
+        atualizarEstadoAtirador();
+        atualizarBarrasVelocidade();
 
-    private void updateAllianceColor() {
-        Optional<Alliance> alliance = DriverStation.getAlliance();
-
-        Color color = Color.kBlack;
-
-        if (alliance.isPresent()) {
-            color = alliance.get() == Alliance.Red ? Color.kFirstRed : Color.kFirstBlue;
-        }
-
-        for (int i = 0; i < allianceView.getLength(); i++) {
-            allianceView.setLED(i, color);
-        }
-    }
-
-    private void updateShooterStatus() {
-        boolean target = hasTarget.getAsBoolean();
-        boolean aligned = isShooterAligned.getAsBoolean();
-        Color statusColor;
-
-        if (aligned && target) {
-            statusColor = Color.kGreen;
-        } else if (target && !aligned) {
-            statusColor = Color.kOrange;
-        } else {
-            double timestamp = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
-            statusColor = (timestamp % 0.5 < 0.25) ? Color.kRed : Color.kBlack;
-        }
-
-        for (int i = 0; i < shooterView.getLength(); i++) {
-            shooterView.setLED(i, statusColor);
-        }
-    }
-
-    private void updateSpeedBar() {
-        double maxSpeed = 4.5;
-        double speed = Math.abs(currentSpeed.getAsDouble());
-        int ledsLit = (int) ((speed / maxSpeed) * boostView.getLength());
-        ledsLit = Math.max(0, Math.min(ledsLit, boostView.getLength()));
-        Color boostColor = Color.kPurple;
-        Color baseColor = Color.kBlack;
-
-        for (int i = 0; i < boostView.getLength(); i++) {
-            if (i < ledsLit) {
-                boostView.setLED(i, boostColor);
-            } else {
-                boostView.setLED(i, baseColor);
-            }
-        }
+        m_led.setData(ledBuffer);
     }
 }
